73e839bd7e1989803cceb88a9027ddb74dfc574a
JavaHunk2020 <javahunk2020@gmail.com>
2023-12-12 05:26:49 +0530

    Protect get all signups against ADMIN role

--- src/main/java/com/technohunk/controller/AuthController.java
+++ src/main/java/com/technohunk/controller/AuthController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -60,6 +61,7 @@ public class AuthController {
 	}
 	
 	@GetMapping("/signups")
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public List<SignupDTO> getSignups() {
 		return signupService.findSignups();
 	}
